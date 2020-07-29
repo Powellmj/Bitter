8cb41082eb9dc7f6c1e718d45635df7e3e0e646e
MichaelPowell <gieringmj@gmail.com>
2020-07-29 12:38:13 -0400

    fix uri

--- client/src/index.js
+++ client/src/index.js
@@ -27,7 +27,7 @@ cache.writeData({
 let uri = "http://localhost:5000/graphql"
 
 if (process.env.NODE_ENV === 'production') {
-  uri = `https://bitter-graphql.herokuapp.com/graphql`;
+  uri = `https://bitter-graphql-v2.herokuapp.com/graphql`;
 }
 
 
